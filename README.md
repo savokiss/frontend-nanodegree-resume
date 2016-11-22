1785d39ae83ac6b5a2808f6eb7aecdcc0f02e439
savokiss <jaynaruto@qq.com>
2016-11-22 23:22:46 +0800

    feature: header

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Project Details
+https://review.udacity.com/#!/rubrics/13/view
 ## How do I complete this project?
 Review the Online Resume [Project Rubric](https://review.udacity.com/?_ga=1.189245867.12280332.1465333852#!/projects/2962818615/rubric).
 
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = 'Savokiss';
+var role = 'Front End Developer';
+var formattedName = HTMLheaderName.replace('%data%', name);
+var formattedRole = HTMLheaderRole.replace('%data%', role);
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);
